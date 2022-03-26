b54ab959aa6363dfc56ef3c42fdff66dc0099e43
neutral2010 <kasiwamoti5@gmail.com>
2022-03-26 18:57:02 +0900

    アカウント編集ページの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = %i[
+      email password password_confirmation remember_me
+      postal_code address self_description
+    ]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
