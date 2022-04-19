a4f46ae5f170e2844652101a1f2998678f539e90
neutral2010 <kasiwamoti5@gmail.com>
2022-04-19 15:53:55 +0900

    FollowRelationモデルとUserモデルのアソシエーションを実装

--- app/models/follow_relation.rb
+++ app/models/follow_relation.rb
@@ -1,3 +1,9 @@
-class FollowRelation < ApplicationRecord
+# frozen_string_literal: true
 
+class FollowRelation < ApplicationRecord
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
+
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_follow_relations, class_name: 'FollowRelation', # 能動的フォロー
+                                     foreign_key: 'follower_id', # フォローしている方のユーザー
+                                     dependent: :destroy, # ユーザーが削除されたら関連も削除
+                                     inverse_of: 'follower'
+  has_many :following, through: :active_follow_relations, source: :followed
+  has_many :reverse_follow_relations, class_name: 'FollowRelation',
+                                      foreign_key: 'followed_id',
+                                      dependent: :destroy,
+                                      inverse_of: 'followed'
+  has_many :followers, through: :reverse_follow_relations, source: :follower
 end
